870b2b5dd5f2f406eb4c4d84110bd020302aa93c
daiju81 <1d07c.m8k.prv@gmail.com>
2020-09-05 00:56:31 +0900

    create e2e test code

---
+++ spec/e2e.spec.js
@@ -0,0 +1,50 @@
+const path = require('path')
+
+describe('E2Eテスト', () => {
+  beforeEach(async () => {
+    await page.goto('file://' + path.resolve(__dirname, '../index.html'))
+  })
+  
+  it('question', async () => {
+    await page.waitForSelector('#question', { visible: true })
+    await expect(page).toMatchElement('#question', {
+      text: 'question',
+    })
+  })
+
+  it('q1', async () => {
+    await page.waitForSelector('#q1', { visible: true })
+    await expect(page).toMatchElement('#q1', {
+      text: 'q1',
+    })
+  })
+
+  it('q2', async () => {
+    await page.waitForSelector('#q2', { visible: true })
+    await expect(page).toMatchElement('#q2', {
+      text: 'q2',
+    })
+  })
+
+  it('q3', async () => {
+    await page.waitForSelector('#q3', { visible: true })
+    await expect(page).toMatchElement('#q3', {
+      text: 'q3',
+    })
+  })
+
+  it('replay', async () => {
+    await page.waitForSelector('a', { visible: true })
+    await expect(page).toMatchElement('a', {
+      text: 'Replay',
+    })
+  })
+
+  it('ボタン', async () => {
+    await page.waitForSelector('#btn', { visible: true })
+    await expect(page).toMatchElement('#btn', {
+      text: 'Next',
+    })
+  })
+
+})
---
+++ src/js/main.js
@@ -0,0 +1,79 @@
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+var question = document.getElementById('question');
+var choices = document.getElementById('choices');
+var btn = document.getElementById('btn');
+var result = document.getElementById('result');
+var scoreLabel = document.querySelector('#result > p');
+var quizSet = shuffle([
+    { q: '世界で一番大きな湖は？', c: ['カスピ海', 'カリブ海', '琵琶湖'] },
+    { q: '2の8乗は？', c: ['256', '64', '1024'] },
+    {
+        q: '次のうち、最初にリリースされた言語は？',
+        c: ['Python', 'JavaScript', 'HTML']
+    },
+]);
+var currentNum = 0;
+var isAnswered;
+var score = 0;
+function shuffle(arr) {
+    var _a;
+    for (var i = arr.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        _a = [arr[i], arr[j]], arr[j] = _a[0], arr[i] = _a[1];
+    }
+    return arr;
+}
+function checkAnswer(li) {
+    if (isAnswered) {
+        return;
+    }
+    isAnswered = true;
+    if (li.textContent === quizSet[currentNum].c[0]) {
+        li.classList.add('correct');
+        score++;
+    }
+    else {
+        li.classList.add('wrong');
+    }
+    btn.classList.remove('disabled');
+}
+function setQuiz() {
+    isAnswered = false;
+    question.textContent = quizSet[currentNum].q;
+    while (choices.firstChild) {
+        choices.removeChild(choices.firstChild);
+    }
+    var shuffledChoices = shuffle(__spreadArrays(quizSet[currentNum].c));
+    shuffledChoices.forEach(function (choice) {
+        var li = document.createElement('li');
+        li.textContent = choice;
+        li.addEventListener('click', function () {
+            checkAnswer(li);
+        });
+        choices.appendChild(li);
+    });
+    if (currentNum === quizSet.length - 1) {
+        btn.textContent = 'Show Score';
+    }
+}
+setQuiz();
+btn.addEventListener('click', function () {
+    if (btn.classList.contains('disabled')) {
+        return;
+    }
+    btn.classList.add('disabled');
+    if (currentNum === quizSet.length - 1) {
+        scoreLabel.textContent = "Score: " + score + " / " + quizSet.length;
+        result.classList.remove('hidden');
+    }
+    else {
+        currentNum++;
+        setQuiz();
+    }
+});
