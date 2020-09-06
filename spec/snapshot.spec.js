69a370b0997208f5a802318da95b101662e4bb22
daiju81 <1d07c.m8k.prv@gmail.com>
2020-09-06 19:11:33 +0900

    add snapshot.spec.js

---
+++ spec/snapshot.spec.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+describe('スナップショットテスト', () => {
+  beforeEach(async () => {
+    await page.goto('file://' + path.resolve(__dirname, '../index_mock.html'))
+  })
+
+  it('HTML/スナップショットテスト', async () => {
+    await page.waitForSelector('body', { visible: true })
+    expect(await page.screenshot()).toMatchSnapshot()
+  })
+})
