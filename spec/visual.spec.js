d81de3e59c860ca1efa141825bb14b13de79357d
daiju81 <1d07c.m8k.prv@gmail.com>
2020-09-06 18:36:21 +0900

    add visual.spec.js

---
+++ spec/visual.spec.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+const { toMatchImageSnapshot } = require("jest-image-snapshot");
+expect.extend({ toMatchImageSnapshot });
+
+describe("ビジュアルテスト", () => {
+  beforeEach(async () => {
+    await page.goto("file://" + path.resolve(__dirname, "../index.html"));
+  });
+
+  it("HTML/CSSを含めた見た目が正しい", async() => {
+    await page.waitForSelector("body", { visible: true });
+    expect(await page.screenshot()).toMatchImageSnapshot();
+  });
+});
+
