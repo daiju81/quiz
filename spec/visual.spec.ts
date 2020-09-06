f9ca3ea8f582771653dcbced856b078d5a73b664
daiju81 <1d07c.m8k.prv@gmail.com>
2020-09-06 22:06:39 +0900

    fix test code

--- spec/visual.spec.ts
+++ spec/visual.spec.ts
@@ -1,6 +1,6 @@
 import path from 'path'
 
-const { toMatchImageSnapshot } = require("jest-image-snapshot")
+import  { toMatchImageSnapshot } from 'jest-image-snapshot'
 expect.extend({ toMatchImageSnapshot })
 
 describe("ビジュアルテスト", () => {
